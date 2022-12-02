921bd35eff2c30a9301222cc6c989051f6a652bc
WS_Boaz <chy5265@hanafos.com>
2022-12-03 03:41:01 +0900

    upgrade

---
+++ js/main.js
@@ -0,0 +1,43 @@
+let target = document.querySelector ("#danamic");
+
+
+    function randomString(){
+        let stringArr = [ "Learn to HTML", "Learn to CSS", "Learn to Javascript", "Learn to Python", "Learn to Ruby"];
+        let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+        let selectStringArr = selectString.split("");
+
+        return selectStringArr;
+    }
+   
+    // 타이핑 리셋
+    function resetTyping(){
+        target.textContent = "";
+        dynamic(randomString());
+    }
+
+    // 한글자씩 text 출력 함수
+    function dynamic(randomArr){
+    
+        /* console.log(randomArr); */
+    
+        if(randomArr.length>0){
+        target.textContent += randomArr.shift();
+        setTimeout(function(){
+            dynamic(randomArr);
+        },80);
+    } else{
+        setTimeout(resetTyping, 2000);
+    }
+}
+
+dynamic(randomString());
+
+/* console.log(selectString);
+console.log(selectStringArr); */
+
+
+function blink(){
+    target.classList.toggle("active");
+}
+setInterval(blink, 500);
+
